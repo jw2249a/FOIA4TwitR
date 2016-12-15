87d44ae0e7fae56067dd2773da08f3655a6b73cb
Jack Williams <jw2249a@users.noreply.github.com>
2016-12-15 04:03:19 -0500

    Add files via upload

---
+++ TwitSelenium.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+path_to_chromedriver = 'C:/Users/jw224/Documents/GitHub/TwitRsucks/TwitRsucks/drivers/chromedriver.exe'
+browser = webdriver.Chrome(executable_path = path_to_chromedriver)
